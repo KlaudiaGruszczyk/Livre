6fff1d5e39fe5071fa5111c2bf5f29de7e0737ed
Klaudia Gru <gruszczyk.klaudia@gmail.com>
2023-03-29 19:17:56 +0200

    Change Login CQRS

--- src/Application/UsersCQRS/Commands/ChangeLogin/ChangeLoginCommandHandler.cs
+++ src/Application/UsersCQRS/Commands/ChangeLogin/ChangeLoginCommandHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace Application.UsersCQRS.Commands.ChangeLogin
+﻿using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.UsersCQRS.Commands.ChangeLogin
 {
-    public class ChangeLoginCommandHandler
+    public class ChangeLoginCommandHandler : IRequestHandler<ChangeLoginCommand, string>
     {
+        private readonly IApplicationDbContext _dbContext;
+
+        public ChangeLoginCommandHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<string> Handle(ChangeLoginCommand command, CancellationToken cancellationToken)
+        {
+            var user = _dbContext.Users.Where(a => a.UserId == command.UserId).FirstOrDefault();
+
+            if (user == null)
+            {
+                return default;
+            }
+            else
+            {
+                user.Login = command.Login;
+                await _dbContext.SaveChangesAsync();
+                return user.Login;
+            }
+        }
     }
 }
