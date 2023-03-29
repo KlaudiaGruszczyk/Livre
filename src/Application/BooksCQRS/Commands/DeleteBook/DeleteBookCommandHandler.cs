05d40cf7a5bbc4372b420f628296c82a3b926e85
Klaudia Gru <gruszczyk.klaudia@gmail.com>
2023-03-29 17:47:17 +0200

    DeleteUser CQRS

--- src/Application/BooksCQRS/Commands/DeleteBook/DeleteBookCommandHandler.cs
+++ src/Application/BooksCQRS/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -1,25 +1,6 @@
-﻿using Application.Common.Interfaces;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-
-namespace Application.BooksCQRS.Commands.DeleteBook
+﻿namespace Application.BooksCQRS.Commands.DeleteBook
 {
-    public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, Guid>
+    public class Class
     {
-        private readonly IApplicationDbContext _dbContext;
-
-        public DeleteBookCommandHandler(IApplicationDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public async Task<Guid> Handle(DeleteBookCommand command, CancellationToken cancellationToken)
-        {
-            var book = await _dbContext.Books.Where(a => a.BookId == command.BookId).FirstOrDefaultAsync();
-            if (book == null) return default;
-            _dbContext.Books.Remove(book);
-            await _dbContext.SaveChangesAsync();
-            return (Guid)book.BookId;
-        }
     }
 }
